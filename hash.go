1df1070ba837e062d156c701fe8f09189cda29ee
Andrei Pelinescu-Onciul <andrei@iptel.org>
2020-11-04 20:12:16 +0100

    copyright spelling fix

--- hash.go
+++ hash.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 Intuitive Labs Gmbh. All rights reserved.
+// Copyright 2019-2020 Intuitive Labs GmbH. All rights reserved.
 //
 // Permission to use, copy, modify, and distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
