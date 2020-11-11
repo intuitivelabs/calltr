6408718f33c0b51d612262a385525e01d4388846
Andrei Pelinescu-Onciul <andrei@iptel.org>
2020-11-11 20:08:36 +0100

    test: fix alloc test
    - free call entries after test allocating them (to avoid
    triggering the bug check for garbage collected and not freed)
    
    - don't test for Key.buf being allocated in the same block as the
    CallEntry, since this would be true (for now) only if build with
     +build alloc_oneblock.

--- alloc_test.go
+++ alloc_test.go
@@ -14,24 +14,31 @@ import (
 
 func TestCallStateAlloc(t *testing.T) {
 
+	const N = 1000000
+	var ce [N]*CallEntry
 	var e *CallEntry = AllocCallEntry(10, 0)
 
 	t.Logf("callstate %p, size %x &buf[0]= %v size %x\n",
 		e, unsafe.Sizeof(*e), &e.Key.buf[0], len(e.Key.buf))
-	i := 0
-	for ; i < 1000000; i++ {
+
+	ce[0] = e
+	i := 1
+	for ; i < N; i++ {
 		sz := uint(rand.Intn(128))
 		e = AllocCallEntry(sz, 0)
+		ce[i] = e
 		if len(e.Key.buf) < int(sz) {
 			t.Errorf("wrong buf size %d, expected at least %d\n",
 				len(e.Key.buf), sz)
 		}
+		/* usefull only in alloc_oneblock mode:
 		if len(e.Key.buf) != 0 &&
 			uintptr(unsafe.Pointer(&(e.Key.buf[0]))) !=
 				uintptr(unsafe.Pointer(e))+unsafe.Sizeof(*e) {
 			t.Errorf("wrong buffer offset %p, e = %p , sizeof(e)=%x\n",
 				&e.Key.buf[0], e, unsafe.Sizeof(*e))
 		}
+		*/
 		for j := 0; j < len(e.Key.buf); j++ {
 			e.Key.buf[j] = 0xff
 		}
@@ -44,6 +51,10 @@ func TestCallStateAlloc(t *testing.T) {
 			t.Errorf("alignment error for e: %p not multiple of %d\n",
 				e, unsafe.Alignof(*e))
 		}
+	}
+	for i = 0; i < N; i++ {
+		FreeCallEntry(ce[i])
+		ce[i] = nil
 	}
 	t.Logf("%d test runs\n", i)
 }
