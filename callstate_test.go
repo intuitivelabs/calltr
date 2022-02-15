135783e2680b7d5c8a4f0c5ea77684eb5f105a6c
Andrei Pelinescu-Onciul <andrei@iptel.org>
2022-02-15 19:20:35 +0100

    test: fixed TestConvStateTimeoutS

--- callstate_test.go
+++ callstate_test.go
@@ -11,9 +11,9 @@ import (
 )
 
 func TestConvStateTimeoutS(t *testing.T) {
-	if len(stateTimeoutS) != int(CallStNonInvFinished)+1 {
+	if len(DefaultConfig.stateTimeoutS) != int(CallStNonInvFinished)+1 {
 		t.Errorf("state to timeout conversion array size mismatch: %d / %d\n",
-			len(stateTimeoutS), int(CallStNonInvFinished)+1)
+			len(DefaultConfig.stateTimeoutS), int(CallStNonInvFinished)+1)
 	}
 }
 
